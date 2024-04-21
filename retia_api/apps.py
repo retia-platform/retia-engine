54ee5f8b3e735902b754349345eeb5730c7868b2
Ezra Lazuardy <ezralazuardy@icloud.com>
2024-04-21 16:39:18 +0700

    feat: update

--- retia_api/apps.py
+++ retia_api/apps.py
@@ -1,13 +1,12 @@
 from django.apps import AppConfig
 
 from retia_api.configurations import settings
+from retia_api.configurations.scheduler import scheduler
 
 
 class RetiaApiConfig(AppConfig):
     name = "retia_api"
 
     def ready(self):
-        from retia_api.configurations.scheduler import scheduler
-
         if settings.SCHEDULER_AUTOSTART:
             scheduler.start()
